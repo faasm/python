24c2deb1b5731581414da097c547e9de427257d8
Simon Shillaker <scs17@ic.ac.uk>
2020-09-21 10:00:55 +0000

    Fix pyfaasm build

--- sanity_check.py
+++ sanity_check.py
@@ -1,6 +1,6 @@
-import distutils.ccompiler
 import sysconfig
 from pprint import pprint
+from os import environ
 
 
 def main():
@@ -13,6 +13,10 @@ def main():
     pprint(sysconfig.get_paths())
     print("Variables")
     pprint(sysconfig.get_config_vars())
+
+    print("Environment")
+    environ_dict = {k: v for k, v in environ.items()}
+    pprint(environ_dict)
 
 
 if __name__ == "__main__":
