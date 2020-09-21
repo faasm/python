324902aef23566f7dd90e86114a8e0df9b3801d7
Simon Shillaker <scs17@ic.ac.uk>
2020-09-21 16:43:58 +0000

    Parallel numpy build and latest submodules

--- install_modules.sh
+++ install_modules.sh
@@ -7,7 +7,9 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Start the cross-env
 source cross_venv/bin/activate
 
-# Numpy
+# Numpy (parallel build https://numpy.org/devdocs/user/building.html)
+export NPY_NUM_BUILD_JOBS=$(nproc --ignore 1)
+echo "Numpy using ${NPY_NUM_BUILD_JOBS} cores"
 pushd ${THIS_DIR}/third-party/numpy >> /dev/null
 pip install .
 popd >> /dev/null
