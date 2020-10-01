8584d07b8a3efe9fd3b06dcfd7539e4cce4cfaa9
Simon Shillaker <scs17@ic.ac.uk>
2020-10-01 06:32:00 +0000

    Added horovod dep, moved scripts to bin

--- bin/install_modules.sh
+++ bin/install_modules.sh
@@ -4,7 +4,6 @@ set -e
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJ_ROOT=${THIS_DIR}/..
-SITE_PACKAGES=${PROJ_ROOT}/cross_venv/cross/lib/python3.8/site-packages
 
 # Start the cross-env
 pushd ${PROJ_ROOT} >> /dev/null
