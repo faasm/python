8a2777881d3de2864091b4a78ee6e285df1d4356
Simon Shillaker <scs17@ic.ac.uk>
2020-10-08 14:46:29 +0000

    Install libffi

--- tasks/ffi.py
+++ tasks/ffi.py
@@ -4,16 +4,15 @@ from subprocess import run
 from faasmcli.util.toolchain import (
     build_config_cmd,
     run_autotools,
+    WASM_SYSROOT,
+    WASM_LIB_INSTALL,
 )
 
 from invoke import task
 
-from os import makedirs
-
 from tasks.env import THIRD_PARTY_DIR, USABLE_CPUS
 
 LIBFFI_DIR = join(THIRD_PARTY_DIR, "libffi")
-PREFIX = join(LIBFFI_DIR, "install")
 
 
 @task(default=True)
@@ -24,9 +23,6 @@ def build(ctx, clean=False):
     if exists(join(LIBFFI_DIR, "Makefile")) and clean:
         run("make clean", cwd=LIBFFI_DIR, shell=True, check=True)
 
-    if not exists(PREFIX):
-        makedirs(PREFIX)
-
     # Autotools
     run_autotools(LIBFFI_DIR)
 
@@ -34,11 +30,10 @@ def build(ctx, clean=False):
     configure_cmd = build_config_cmd(
         [
             "./configure",
-            "--prefix={}".format(PREFIX),
+            "--prefix={}".format(WASM_SYSROOT),
             "--disable-multi-os-directory",
             "--disable-docs",
         ],
-        shared=True,
     )
     configure_cmd = " ".join(configure_cmd)
     run(configure_cmd, check=True, shell=True, cwd=LIBFFI_DIR)
@@ -46,3 +41,13 @@ def build(ctx, clean=False):
     # Run make
     make_cmd = ["make", "-j {}".format(USABLE_CPUS)]
     run(" ".join(make_cmd), shell=True, check=True, cwd=LIBFFI_DIR)
+
+    # Run install (although apparently doesn't do anything)
+    run("make install", shell=True, check=True, cwd=LIBFFI_DIR)
+
+    # Ensure the lib is copied into place
+    src_lib = join(LIBFFI_DIR, "wasm32-unknown-wasi", ".libs", "libffi.a")
+    dest_lib = join(WASM_LIB_INSTALL, "libffi.a")
+    print("Copying {} to {}".format(src_lib, dest_lib))
+    run("cp {} {}".format(src_lib, dest_lib), shell=True, check=True) 
+
