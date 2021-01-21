5e7e248c6d24d77b1dda53e3380e79bcc6039bf8
Simon Shillaker <scs17@ic.ac.uk>
2021-01-21 18:12:04 +0000

    Removed unused task

--- tasks/runtime.py
+++ tasks/runtime.py
@@ -1,7 +1,7 @@
 import glob
 
 from os.path import join, exists
-from shutil import rmtree, copytree
+from shutil import rmtree
 from subprocess import run
 from os import makedirs, remove
 
@@ -13,17 +13,12 @@ CPYTHON_SRC = join(THIRD_PARTY_DIR, "cpython")
 CPYTHON_INSTALL_DIR = join(CPYTHON_SRC, "install", "wasm")
 CROSSENV_WASM_DIR = join(PROJ_ROOT, "cross_venv", "cross")
 
-INCLUDE_ROOT = join(FAASM_RUNTIME_ROOT, "include")
-LIB_ROOT = join(FAASM_RUNTIME_ROOT, "lib")
-
-INCLUDE_SRC_DIR = join(CPYTHON_INSTALL_DIR, "include", "python3.8")
 LIB_SRC_DIR = join(CPYTHON_INSTALL_DIR, "lib", "python3.8")
+LIB_DEST_DIR = join(FAASM_RUNTIME_ROOT, "lib", "python3.8")
+
 SITE_PACKAGES_SRC_DIR = join(
     CROSSENV_WASM_DIR, "lib", "python3.8", "site-packages"
 )
-
-INCLUDE_DEST_DIR = join(INCLUDE_ROOT, "python3.8")
-LIB_DEST_DIR = join(LIB_ROOT, "python3.8")
 SITE_PACKAGES_DEST_DIR = join(LIB_DEST_DIR, "site-packages")
 
 
@@ -45,47 +40,15 @@ def _clear_pyc_files(dir_path):
     _glob_remove(pycache_glob, recursive=True, directory=True)
 
 
-@task
-def cpython(ctx):
-    # Remove dirs to be replaced by those we copy in
-    if exists(INCLUDE_DEST_DIR):
-        print("Removing {}".format(INCLUDE_DEST_DIR))
-        rmtree(INCLUDE_DEST_DIR)
-
-    if exists(LIB_DEST_DIR):
-        print("Removing {}".format(LIB_DEST_DIR))
-        rmtree(LIB_DEST_DIR)
-
-    # Copy CPython includes
-    print("Copying {} to {}".format(INCLUDE_SRC_DIR, INCLUDE_DEST_DIR))
-    copytree(INCLUDE_SRC_DIR, INCLUDE_DEST_DIR)
-
-    # Copy CPython libs
-    print("Copying {} to {}".format(LIB_SRC_DIR, LIB_DEST_DIR))
-    copytree(LIB_SRC_DIR, LIB_DEST_DIR)
-
-
 @task(default=True)
 def copy(ctx):
     """
-    Copies the CPython archive and all installed modules from the cpython build
-    into the Faasm runtime root
+    Copies the installed modules into the Faasm runtime root
     """
-    if not exists(LIB_ROOT):
-        print("Creating {}".format(LIB_ROOT))
-        makedirs(LIB_ROOT)
-
-    if not exists(INCLUDE_ROOT):
-        print("Creating {}".format(INCLUDE_ROOT))
-        makedirs(INCLUDE_ROOT)
-
     # Clear out pyc files
     print("Clearing out pyc files")
     _clear_pyc_files(LIB_SRC_DIR)
     _clear_pyc_files(SITE_PACKAGES_SRC_DIR)
-
-    # Copy CPython
-    cpython()
 
     # Copy cross-compiled modules
     if not exists(SITE_PACKAGES_DEST_DIR):
