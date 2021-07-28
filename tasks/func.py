0241a9ce949d9a441f0d271e11a505fbc30a9c39
Simon Shillaker <scs17@ic.ac.uk>
2021-07-28 13:27:43 +0000

    Adding remote upload for funds

--- tasks/func.py
+++ tasks/func.py
@@ -1,8 +1,10 @@
 from faasmtools.compile_util import wasm_cmake
 from faasmtools.env import WASM_DIR
+from faasmtools.build import FAASM_LOCAL_DIR
+
+import requests
 from os.path import join
 from invoke import task
-from faasmtools.build import FAASM_LOCAL_DIR
 from os import makedirs, listdir
 from shutil import copy
 from tasks.env import PROJ_ROOT
@@ -31,30 +33,45 @@ def compile(ctx, clean=False, debug=False):
     wasm_cmake(FUNC_DIR, FUNC_BUILD_DIR, FUNC, clean, debug)
 
     # Copy the wasm into place
-
     makedirs(DEST_FOLDER, exist_ok=True)
     copy(WASM_FILE, DEST_FILE)
 
 
 @task
-def uploadpy(ctx, local=False):
+def upload(ctx, host="upload", port=8002):
+    url = "http://{}:{}/f/{}/{}".format(host, port, USER, FUNC)
+    response = requests.put(url, data=open(WASM_FILE, "rb"))
+
+    print("Response {}: {}".format(response.status_code, response.text))
+
+
+@task
+def uploadpy(ctx, local=False, host="upload", port=8002):
     """
     Upload functions written in Python
     """
-    if not local:
-        raise RuntimeError("Remote upload not yet implemented")
-
-    makedirs(PY_UPLOAD_DIR, exist_ok=True)
-
     # Get all Python funcs
     funcs = listdir(PY_FUNC_DIR)
     funcs = [f for f in funcs if f.endswith(".py")]
     funcs = [f.replace(".py", "") for f in funcs]
 
     for func in funcs:
-        func_upload_dir = join(PY_UPLOAD_DIR, func)
-        makedirs(func_upload_dir, exist_ok=True)
-
         src_file = join(PY_FUNC_DIR, "{}.py".format(func))
-        dest_file = join(func_upload_dir, "function.py")
-        copy(src_file, dest_file)
+
+        if local:
+            makedirs(PY_UPLOAD_DIR, exist_ok=True)
+            func_upload_dir = join(PY_UPLOAD_DIR, func)
+            makedirs(func_upload_dir, exist_ok=True)
+
+            dest_file = join(func_upload_dir, "function.py")
+
+            copy(src_file, dest_file)
+        else:
+            url = "http://{}:{}/p/{}/{}".format(host, port, "python", func)
+            response = requests.put(url, data=open(src_file, "rb"))
+
+            print(
+                "{} response {}: {}".format(
+                    func, response.status_code, response.text
+                )
+            )
