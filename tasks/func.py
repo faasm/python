b434d9579e9d67a6ff6abe5ab54ab5c8d60867e7
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2021-08-02 09:58:28 +0200

    Updates for latest Faasm (#12)
    * Update to removal of shared_store dir
    
    * Adding remote upload for funds
    
    * Add invoke and upload functions
    
    * Add logging for local function upload
    
    * Fix printing error

--- tasks/func.py
+++ tasks/func.py
@@ -1,8 +1,10 @@
 from faasmtools.compile_util import wasm_cmake
 from faasmtools.env import WASM_DIR
+from faasmtools.build import FAASM_LOCAL_DIR
+
+import requests
 from os.path import join
 from invoke import task
-from faasmtools.build import FAASM_LOCAL_DIR
 from os import makedirs, listdir
 from shutil import copy
 from tasks.env import PROJ_ROOT
@@ -17,7 +19,7 @@ WASM_FILE = join(FUNC_BUILD_DIR, "py_func.wasm")
 DEST_FOLDER = join(WASM_DIR, USER, FUNC)
 DEST_FILE = join(DEST_FOLDER, "function.wasm")
 
-FAASM_SHARED_STORAGE_ROOT = join(FAASM_LOCAL_DIR, "shared_store")
+FAASM_SHARED_STORAGE_ROOT = join(FAASM_LOCAL_DIR, "shared")
 PY_FUNC_DIR = join(PROJ_ROOT, "func", "python")
 PY_UPLOAD_DIR = join(FAASM_SHARED_STORAGE_ROOT, "pyfuncs", "python")
 
@@ -31,30 +33,76 @@ def compile(ctx, clean=False, debug=False):
     wasm_cmake(FUNC_DIR, FUNC_BUILD_DIR, FUNC, clean, debug)
 
     # Copy the wasm into place
-
     makedirs(DEST_FOLDER, exist_ok=True)
     copy(WASM_FILE, DEST_FILE)
 
 
 @task
-def uploadpy(ctx, local=False):
+def upload(ctx, host="upload", port=8002):
     """
-    Upload functions written in Python
+    Upload the CPython function
     """
-    if not local:
-        raise RuntimeError("Remote upload not yet implemented")
+    url = "http://{}:{}/f/{}/{}".format(host, port, USER, FUNC)
+    print("Uploading {}/{} to {}".format(USER, FUNC, url))
+    response = requests.put(url, data=open(WASM_FILE, "rb"))
 
-    makedirs(PY_UPLOAD_DIR, exist_ok=True)
+    print("Response {}: {}".format(response.status_code, response.text))
 
-    # Get all Python funcs
+
+@task
+def uploadpy(ctx, func, local=False, host="upload", port=8002):
+    src_file = join(PY_FUNC_DIR, "{}.py".format(func))
+
+    if local:
+        makedirs(PY_UPLOAD_DIR, exist_ok=True)
+        func_upload_dir = join(PY_UPLOAD_DIR, func)
+        makedirs(func_upload_dir, exist_ok=True)
+
+        dest_file = join(func_upload_dir, "function.py")
+        print("Copying function {} {} -> {}".format(func, src_file, dest_file))
+        copy(src_file, dest_file)
+    else:
+        url = "http://{}:{}/p/{}/{}".format(host, port, "python", func)
+        response = requests.put(url, data=open(src_file, "rb"))
+
+        print(
+            "{} response {}: {}".format(
+                func, response.status_code, response.text
+            )
+        )
+
+
+@task
+def upload_all(ctx, local=False, host="upload", port=8002):
+    """
+    Upload all Python functions
+    """
     funcs = listdir(PY_FUNC_DIR)
     funcs = [f for f in funcs if f.endswith(".py")]
     funcs = [f.replace(".py", "") for f in funcs]
 
     for func in funcs:
-        func_upload_dir = join(PY_UPLOAD_DIR, func)
-        makedirs(func_upload_dir, exist_ok=True)
+        uploadpy(ctx, func, local=local, host=host, port=port)
 
-        src_file = join(PY_FUNC_DIR, "{}.py".format(func))
-        dest_file = join(func_upload_dir, "function.py")
-        copy(src_file, dest_file)
+
+@task
+def invoke(ctx, user, func, input_data=None, host="worker", port=8080):
+    """
+    Invoke a function
+    """
+
+    url = "http://{}:{}".format(host, port)
+    data = {
+        "user": USER,
+        "function": FUNC,
+        "py_user": user,
+        "py_func": func,
+        "python": True,
+    }
+
+    if input_data:
+        data["input_data"] = input_data
+
+    response = requests.post(url, json=data)
+
+    print("Response {}:\n{}".format(response.status_code, response.text))
